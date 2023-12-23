61909b3af043c3d3b781fc10e6b4b921e61abff2
Ceymikey <misakabaali@gmail.com>
2023-12-23 19:30:19 +0100

    Changed Frontend aspects + Fixed many `Bugs and issues`

--- _dirname.js
+++ _dirname.js
@@ -1,4 +1,4 @@
 const path = require('path');
 
-const indexPath = path.join(__dirname, 'src', 'index.html');
+const indexPath = path.join(__dirname, 'index.html');
 console.log('Path to index.html:', indexPath);
