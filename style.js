4243a9382de1d3ce61fd340743e2d7cdbb5f5ff4
Ceymikey <misakabaali@gmail.com>
2024-01-10 18:18:50 +0100

    Changed main website layout, fixed annoying bugs, added new section to main: `contributors`

--- style.js
+++ style.js
@@ -1,4 +1,4 @@
-// observes the html section elements and performs a animation on scroll
+// observes the HTML section elements and performs an animation on scroll
 const observer = new IntersectionObserver((entries => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -11,9 +11,17 @@ const observer = new IntersectionObserver((entries => {
 }));
 
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el))
+hiddenElements.forEach((el) => observer.observe(el));
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Check if the page was reloaded
+    if (performance.navigation.type === 1) {
+        // Page was reloaded, fade in the element
+        var fadeInElement = document.getElementById('fadeInElement');
+        fadeInElement.style.display = 'block';  // Show the element
+        fadeInElement.style.opacity = '1';      // Set opacity to 1 for the fade-in effect
+    }
+
     var scrollButton = document.getElementById('scrollButton');
     var scrollSection = document.getElementById('scrollSection');
 
