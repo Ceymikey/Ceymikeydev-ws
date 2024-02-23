6824564b5ddc89f3463353fa267d71d029f6ee76
Ceymikey <misakabaali@gmail.com>
2024-02-23 23:38:33 +0100

    fix: Fixed assets not displaying on user profile

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-// Serve static files from the root directory
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Start the server
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/pages/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/admin.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
