832012f24ee13e36d63d370b18194265df54d245
Ceymikey <misakabaali@gmail.com>
2024-03-03 00:00:01 +0100

    chore: Changed portfolio section to refer to new link

--- style.js
+++ style.js
@@ -1,9 +1,10 @@
-// observes the HTML section elements and performs an animation on scroll
 const observer = new IntersectionObserver((entries => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
         }
     });
 }));
@@ -12,12 +13,10 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Check if the page was reloaded
     if (performance.navigation.type === 1) {
-        // Page was reloaded, fade in the element
         var fadeInElement = document.getElementById('fadeInElement');
-        fadeInElement.style.display = 'block';  // Show the element
-        fadeInElement.style.opacity = '1';      // Set opacity to 1 for the fade-in effect
+        fadeInElement.style.display = 'block';
+        fadeInElement.style.opacity = '1';
     }
 
     var scrollButton = document.getElementById('scrollButton');
